4e4fe5634a59f46f2511b212d95b844e2e8059e4
Rekxis <Rekxis@gmx.de>
2023-07-24 12:53:41 +0200

    whitespaces and format

--- js/view_controller.js
+++ js/view_controller.js
@@ -3,22 +3,22 @@
 
 /* Der Plan
     Einlesen Daten von Webseite :: check!
-	Check Daten :: check!
+    Check Daten :: check!
     Btn. Trigger :: check!
     Business-Logic (Alter --> Getränk) :: check!
     Bild austauschen :: check!
 */
 
-/***  Ablaufsteuerung | Control */  
+/***  Ablaufsteuerung | Control */
 // 5. Modul Ablaufsteuerung | Test:
 // controller()
 function controller() {
-  output(updateImg(checkAge(getInput()))) 
+    output(updateImg(checkAge(getInput())))
 }
 
 // 5a. Trigger - BtnClick 
 const btn = document.getElementById("trigBtn");
-btn.addEventListener("click",actOnClick);
+btn.addEventListener("click", actOnClick);
 
 // 5b. Event-Dispatcher
 function actOnClick() {
@@ -29,7 +29,7 @@ function actOnClick() {
     }
 }
 
-/***  Eingabe | Check auf Datenintegrität */  
+/***  Eingabe | Check auf Datenintegrität */
 
 //4. Definiton : Eingabefeld
 const field = document.getElementsByName("eingabe")[0];
@@ -40,24 +40,24 @@ function getInput() {
 }
 
 // 4b. Trigger-Input
-field.addEventListener("input",isInputValid);
+field.addEventListener("input", isInputValid);
 
 // 4c. Check auf korrekte Eingaben ...
 function isInputValid() {
-   
-    let inputStr    = field.value;
-    const patt      = /^[0-9]{1,3}$/g; 
-    let cond        = patt.test(inputStr);
+
+    let inputStr = field.value;
+    const patt = /^[0-9]{1,3}$/g;
+    let cond = patt.test(inputStr);
 
     if (!cond) {
         field.value = "";
         updateImg(data.default.bev);
     }
-   
+
     return cond;
 }
 
-/***  Business-Logic */ 
+/***  Business-Logic */
 //3. Modul: Business-Logic (Mapping) | Test:
 // output(checkAge(2));
 // output(checkAge(7));
@@ -67,20 +67,20 @@ function isInputValid() {
 // output(checkAge(135));
 function checkAge(age) {
     switch (true) {
-        case (age >= data.milk.lower) && (age <= data.milk.upper) :
+        case (age >= data.milk.lower) && (age <= data.milk.upper):
             return data.milk.bev;
-        case (age >= data.juice.lower) && (age <= data.juice.upper) :
+        case (age >= data.juice.lower) && (age <= data.juice.upper):
             return data.juice.bev;
-        case (age >= data.cola.lower) && (age <= data.cola.upper) :
+        case (age >= data.cola.lower) && (age <= data.cola.upper):
             return data.cola.bev;
-        case (age >= data.wine.lower) && (age <= data.wine.upper) :
+        case (age >= data.wine.lower) && (age <= data.wine.upper):
             return data.wine.bev;
         default:
-            return data.default.bev;           
+            return data.default.bev;
     }
 }
 
-/*** View-Schicht aktualisieren */ 
+/*** View-Schicht aktualisieren */
 //2. Modul: Bild aktualisieren | Test:
 // output(updateImg("cola"));
 // output(updateImg("milch"));
